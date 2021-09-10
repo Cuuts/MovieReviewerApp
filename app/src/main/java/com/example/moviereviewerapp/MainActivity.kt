3f10dc31dca8f6f874b02f41603d66f759a38e47
Tobias Andersen <tobi3146@gmail.com>
2021-09-10 12:22:53 +0200

    Fredag d. 10/9 middags commit
    Tilføjet dynamisk genererede average rating under filmtitler i MainActivity.kt
    Andre diverse småjusteringer i UI

--- app/src/main/java/com/example/moviereviewerapp/MainActivity.kt
+++ app/src/main/java/com/example/moviereviewerapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
+import android.widget.RatingBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.moviereviewerapp.databinding.ActivityMainBinding
@@ -39,20 +40,29 @@ class MainActivity : AppCompatActivity() {
          * This for loop iterates over every movie in testData
          * On every loop a radio button is created with a unique id, and its text being that of the current movie in testData
          * Then the current radio button is added to the radio group view
+         * Along with every radio button, a non-interactable rating bar is also created with a specific size, a unique id and later the average rating if avaiable
          *
          * If the list with updated movie data isn't empty (null) we first sanity check by checking that the updated list isn't out of order
          * Then we update arrayList at the proper index with the same index in the list of all reviews
+         * It is also here the average rating is fetched
          */
         for ((count, i) in testData.withIndex()) {
             val rb = RadioButton(this)
+            val rb2 = RatingBar(this)
             rb.text = i.title
             rb.id = count
+            rb2.setIsIndicator(true)
+            rb2.scaleX = .5f
+            rb2.scaleY = .5f
+            rb2.id = count
 
             binding.rgMovies.addView(rb)
+            binding.rgMovies.addView(rb2)
 
             if (item != null) {
                 if (i.title == item[count].title) {
                     arrayList[count] = item[count]
+                    rb2.rating = averageRating(item[count])
                 }
             }
         }
@@ -83,6 +93,15 @@ class MainActivity : AppCompatActivity() {
             putParcelableArrayListExtra("EXTRA_MOVIE_DATA", movieDataInput)
         }
         startActivity(intent)
+    }
+
+    private fun averageRating(movie: Movie): Float {
+        val size = movie.reviewList.size
+        var sum = 0f
+        for (i in movie.reviewList) {
+            sum += i.rating
+        }
+        return sum/size
     }
 
 }
